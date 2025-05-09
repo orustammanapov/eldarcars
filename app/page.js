e8a2a5dbb29b4f339c9d0ddef0c1148e3cfe2f2f
orustammanapov <orustammanapov@gmail.com>
2025-05-10 00:14:13 +0200

    use pocketbase API to retrieve cars for listing instead of using dummy data

--- app/page.js
+++ app/page.js
@@ -5,38 +5,54 @@ const currency = new Intl.NumberFormat('nl-NL', {
   currency: 'EUR',
 });
 
-const cars = [
-  {
-    brand: 'Bentley',
-    model: '8 Litre',
-    price: currency.format(5_454),
-    image: '/images/569x426/bentley-8-litre-best.jpg',
-    slug: 'bentley-8-litre-best', // 'https://eldarcars.com/101/view_vehicle/49/stadsauto/15/454',
-  },
-  {
-    brand: 'Audi',
-    model: 'Coupe', // "34",
-    price: currency.format(454_545),
-    image: '/images/569x426/audi-coupe-modest.jpg',
-    slug: 'audi-coupe-modest', // "https://eldarcars.com/101/view_vehicle/49/stadsauto/12/34",
-  },
-  {
-    brand: 'Mercedes Benz',
-    model: 'GL 350 BlueTEC 4Matic AMG',
-    price: currency.format(545_454),
-    image: '/images/569x426/mercedes-gl-350-bluetec-4matic-amg.jpg',
-    slug: 'mercedes-gl-350-bluetec-4matic-amg', // "https://eldarcars.com/101/view_vehicle/52/sub-of-terreinwagen/10/mercedes-gl-350-bluetec-4matic-amg",
-  },
-  {
-    brand: 'Mercedes',
-    model: 'Menz E 250 CDi Coupé',
-    price: currency.format(9_500),
-    image: "/images/569x426/mercedes-menz-e-250-cdi-coupe.jpg",
-    slug: "mercedes-menz-e-250-cdi-coupe", // "https://eldarcars.com/101/view_vehicle/50/coupe/9/mercedes-menz-e-250-cdi-coup%C3%A9",
-  },
-];
+// const cars = [
+//   {
+//     brand: 'Bentley',
+//     model: '8 Litre',
+//     price: currency.format(5_454),
+//     image: '/images/569x426/bentley-8-litre-best.jpg',
+//     slug: 'bentley-8-litre-best', // 'https://eldarcars.com/101/view_vehicle/49/stadsauto/15/454',
+//   },
+//   {
+//     brand: 'Audi',
+//     model: 'Coupe', // "34",
+//     price: currency.format(454_545),
+//     image: '/images/569x426/audi-coupe-modest.jpg',
+//     slug: 'audi-coupe-modest', // "https://eldarcars.com/101/view_vehicle/49/stadsauto/12/34",
+//   },
+//   {
+//     brand: 'Mercedes Benz',
+//     model: 'GL 350 BlueTEC 4Matic AMG',
+//     price: currency.format(545_454),
+//     image: '/images/569x426/mercedes-gl-350-bluetec-4matic-amg.jpg',
+//     slug: 'mercedes-gl-350-bluetec-4matic-amg', // "https://eldarcars.com/101/view_vehicle/52/sub-of-terreinwagen/10/mercedes-gl-350-bluetec-4matic-amg",
+//   },
+//   {
+//     brand: 'Mercedes',
+//     model: 'Menz E 250 CDi Coupé',
+//     price: currency.format(9_500),
+//     image: "/images/569x426/mercedes-menz-e-250-cdi-coupe.jpg",
+//     slug: "mercedes-menz-e-250-cdi-coupe", // "https://eldarcars.com/101/view_vehicle/50/coupe/9/mercedes-menz-e-250-cdi-coup%C3%A9",
+//   },
+// ];
 
-export default function Home() {
+export default async function Home() {
+
+    const res = await fetch("http://localhost:8090/api/collections/cars/records", {
+    cache: "no-store", // or use "force-cache" for ISR
+  });
+
+  const data = await res.json();
+
+  // PocketBase returns { items: [...] }
+  const cars = data.items.map(car => ({
+    brand: car.brand,
+    model: car.model,
+    price: currency.format(car.priceAmount),
+    image: car.images?.[0] ? `http://localhost:8090/api/files/cars/${car.id}/${car.images[0]}` : '/placeholder.jpg',
+    slug: car.slug,
+  }));
+
   return (
     <section className="grid xl:grid-cols-3 md:grid-cols-2 gap-4">
 
