67adf4fa5c147ccf46234e527dc01f7648d64ce5
orustammanapov <orustammanapov@gmail.com>
2025-05-12 23:11:34 +0200

    add status ribbon

--- app/page.js
+++ app/page.js
@@ -41,13 +41,21 @@ export default async function Home() {
 
   const carsData = await pb.collection('cars').getFullList({
     sort: '-created', // Optional: sort by creation time or any field
+    expand: "status",
   });
+
+  const banner = {
+    available: "",
+    reserved: "bg-yellow-500",
+    sold: "bg-red-600",
+  }
 
   const cars = carsData.map(car => ({
     title: car.title,
     price: currency.format(car.priceAmount),
     image: car.images?.[0] ? `http://localhost:8090/api/files/cars/${car.id}/${car.images[0]}` : '/placeholder.jpg',
     slug: car.slug,
+    status: car.expand.status.value,
   }));
 
   return (
@@ -55,18 +63,26 @@ export default async function Home() {
 
       {/* https://react.dev/learn/rendering-lists#keeping-list-items-in-order-with-key */}
       {cars.map(car =>        
-        <article key={car.slug} className="bg-gray-900 rounded-md overflow-hidden">
-          {/* hover:text-red-500 */}
+        <article key={car.slug} className="relative bg-gray-900 rounded-md overflow-hidden">
+
+          {/* Status Ribbon */}
+          {car.status !== "available" && (
+            <div className={`absolute top-8 -right-12 w-48 transform rotate-45 text-white text-center py-1 shadow-lg z-10
+              ${banner[car.status]}`}>
+              {car.status.toUpperCase()}
+            </div>
+          )}
+
           <Link href={`/cars/${car.slug}`} className="flex flex-col font-medium text-gray-300 hover:text-gray-200">
             <figure>
-              <img src={car.image} alt={car.title} />
+              <img src={car.image} alt={car.title} className="w-full object-cover" />
               <figcaption className="flex justify-between p-4">
                 <span className="text-left">{car.price}</span>
                 <span className="text-right">{car.title}</span>
               </figcaption>
             </figure>
           </Link>
-        </article>        
+        </article>
       )}
     </section>
   );
