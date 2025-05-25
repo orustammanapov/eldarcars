0dfecf28b598da25fe2a76c875c2a1de2abbc4d6
orustammanapov <orustammanapov@gmail.com>
2025-05-25 12:46:35 +0200

    disable auto cancellation to fix cloudflare deployment

--- app/pocketbase.js
+++ app/pocketbase.js
@@ -1,3 +1,6 @@
 import PocketBase from "pocketbase";
 
-export const pb = new PocketBase(process.env.NEXT_PUBLIC_API_URL);
+const pb = new PocketBase(process.env.NEXT_PUBLIC_API_URL);
+pb.autoCancellation(false); // disables abort on client reuse
+
+export { pb };
